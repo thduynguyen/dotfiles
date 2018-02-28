ed81d70e8171c680ce5ec62d769badfc0af87ba3
Duy-Nguyen Ta <duy@tri.global>
2018-02-28 16:23:29 -0500

    fix zsh install script

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -1,4 +1,5 @@
 apt install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-ln -sr zhsrc ../../.zhsrc
+rm ~/.zshrc
+ln -sr ./zshrc ../../.zshrc
 cp *.zsh-theme ~/.oh-my-zsh/themes/
