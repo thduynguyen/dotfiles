155705f75e3abb75cbc32bebef0b95ecf538cbbe
Duy-Nguyen Ta <thduynguyen@gmail.com>
2022-06-24 22:56:04 -0400

    better bash info and debug (untested)

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -x
+set -euxo pipefail
+
 if [ $(id -u) = 0 ]; then
    echo "Should not run as root."
    exit 1
@@ -9,11 +11,13 @@ sudo apt install zsh curl
 # After installed, shell will switch to zsh. Press Ctrl-D to exit and continue.
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+echo "Press Ctrl-D to exit the new zsh shell and continue..."
+
 rm ~/.zshrc
 ln -sr ./zshrc ../../.zshrc
 cp *.zsh-theme ~/.oh-my-zsh/themes/
 
-# Install custom plugins
+echo "Installing custom plugins..."
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
